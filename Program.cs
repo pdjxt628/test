0d28c08b9e6980c184ab56935cfea5eb2cac0917
pdjxt628 <pdjxt628@gmail.com>
2023-07-05 12:09:29 +0900

    最初のプッシュ

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace mongo_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionUri = "mongodb+srv://makoto:ae21215926@cluster0.zihxmz9.mongodb.net/?retryWrites=true&w=majority";
+
+            var settings = MongoClientSettings.FromConnectionString(connectionUri);
+
+            var client = new MongoClient(settings);
+            var db = client.GetDatabase("test");
+            var collection = db.GetCollection<BsonDocument>("test1");
+            var result = collection.Find("{ID:1}").FirstOrDefault();
+            Console.WriteLine(result["text"]);
+
+        }
+    }
+}
+
+
+
